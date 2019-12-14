cd77bc71e2e659dac2c54ee8ad52033a28e0b2d3
newham <newham.cn@gmail.com>
2019-12-14 12:35:37 +0800

    add test cases

--- test/tps/test.go
+++ test/tps/test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
@@ -25,6 +26,16 @@ func main() {
 			Loop(1, n, TestDCAddURL)
 		} else if fName == "GetURL" {
 			Loop(1, n, TestDCGetURL)
+		}
+	} else if ccName == "pc" {
+		if fName == "AddPolicy" {
+			Loop(1, n, TestPCAddPolicy)
+		} else if fName == "GetPolicy" {
+			Loop(1, n, TestPCGetPolicy)
+		} else if fName == "DeletePolicy" {
+			Loop(1, n, TestPCDeletePolicy)
+		} else if fName == "UpdatePolicy" {
+			Loop(1, n, TestPCUpdatePolicy)
 		}
 	} else {
 		println("please input [cc_name] [f_name] [n]")
@@ -62,6 +73,35 @@ func Loop(loop int, n int, f func(c chan int, n int)) {
 	}
 }
 
+func TestPCAddPolicy(c chan int, n int) {
+	i := 0
+	hamtask.NewSimpleWorker(n, func(i int, d hamtask.Data) {
+		url := d.String()
+		_, err := http.Get(url)
+		if err != nil {
+			log.Println("Error", err.Error())
+			return
+		}
+		// println(resp.Status)
+
+	}, func() hamtask.Data {
+		i++
+		now := time.Now().Unix()
+		policyStr := fmt.Sprintf(`{"AS":{"userId":"%s","role":"u1","group":"g1"},"AO":{"deviceId":"%s","MAC":"%s"},"AP":1,"AE":{"createdTime":%d,"endTime":%d,"allowedIP":"*.*.*.*"}}`, GetUserID(i), GetDeviceID(i), RandomMac(), now, now+100000)
+		return hamtask.String(GetURL("http://localhost:8001/fabric-iot/test", "pc", "AddPolicy", policyStr))
+	}, n).Start()
+	c <- 1
+}
+
+func TestPCGetPolicy(c chan int, n int) {
+}
+
+func TestPCDeletePolicy(c chan int, n int) {
+}
+
+func TestPCUpdatePolicy(c chan int, n int) {
+}
+
 func TestDCGetURL(c chan int, n int) {
 	i := 0
 	hamtask.NewSimpleWorker(n, func(i int, d hamtask.Data) {
@@ -75,7 +115,7 @@ func TestDCGetURL(c chan int, n int) {
 
 	}, func() hamtask.Data {
 		i++
-		return hamtask.String(GetURL("http://localhost:8001/fabric-iot/test", "dc", "GetURL", strconv.Itoa(i)))
+		return hamtask.String(GetURL("http://localhost:8001/fabric-iot/test", "dc", "GetURL", GetDeviceID(i)))
 	}, n).Start()
 	c <- 1
 }
@@ -93,11 +133,40 @@ func TestDCAddURL(c chan int, n int) {
 
 	}, func() hamtask.Data {
 		i++
-		return hamtask.String(GetURL("http://localhost:8001/fabric-iot/test", "dc", "AddURL", strconv.Itoa(i), "https://test.res"))
+		return hamtask.String(GetURL("http://localhost:8001/fabric-iot/test", "dc", "AddURL", GetDeviceID(i), "https://test"+GetDeviceID(i)+".res"))
 	}, n).Start()
 	c <- 1
 }
 
+func GetID(i int) int {
+	return i + 10000
+}
+
+func GetUserID(i int) string {
+	return strconv.Itoa(GetID(i))
+}
+
+func GetDeviceID(i int) string {
+	return fmt.Sprintf("D%d", GetID(i))
+}
+
 func GetURL(url, cc_name, f_name string, args ...string) string {
 	return fmt.Sprintf("%s?cc_name=%s&f_name=%s&args=[%s]", url, cc_name, f_name, strings.Join(args, ","))
+}
+
+func RandomX(N, n int) string {
+	ip := []string{}
+	for i := 0; i < N; i++ {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		ip = append(ip, strconv.FormatInt(int64(r.Intn(n)), 16))
+	}
+	return strings.Join(ip, ":")
+}
+
+func RandomMac() string {
+	return RandomX(6, 64)
+}
+
+func RandomIPv6() string {
+	return RandomX(8, 65536)
 }
