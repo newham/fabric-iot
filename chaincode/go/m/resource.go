3c8c833f260627f1ddfd25139871dee3004d0c07
CUMT-lty <45425948+CUMT-lty@users.noreply.github.com>
2023-10-02 14:16:29 +0800

    Update resource.go

--- chaincode/go/m/resource.go
+++ chaincode/go/m/resource.go
@@ -2,22 +2,42 @@ package m
 
 import "encoding/json"
 
-// Define the resource structure
-type Resource struct {
-	Timestamp int64  `json:"timestamp"`
-	URL       string `json:"url"`
+// Define the model resource structure
+type TierModel struct { // 层聚合模型
+	ModelID        string `json:"ModelID"`
+	ModelType      int `json:"ModelType"`       // 0: 层聚合模型
+	ModelTier      int `json:"ModelTier"`
+	TierRound      int `json:"TierRound"`
+	GlobalRound    int `json:"GlobalRound"`
+	ModelEncryAddr string `json:ModelEncryAddr` // 模型的 IPFS 保存地址
 }
 
-func (r Resource) ToBytes() []byte {
-	b, err := json.Marshal(r)
+type GlobalModel struct { // 全局聚合模型
+	ModelID        string `json:"ModelID"`
+	ModelType      int `json:"ModelType"`       // 1: 代表全局聚合模型
+	GlobalRound    int `json:"GlobalRound"`
+	ModelEncryAddr string `json:ModelEncryAddr` // 模型的 IPFS 保存地址
+}
+
+func (tm TierModel) ToBytes() []byte {
+	bs, err := json.Marshal(tm)
 	if err != nil {
 		return nil
 	}
-	return b
+	return bs
 }
 
-func NewResource(b []byte) (Resource, error) {
-	r := Resource{}
-	err := json.Unmarshal(b, &r)
-	return r, err
+func (gm GlobalModel) ToBytes() []byte {
+	bs, err := json.Marshal(gm)
+	if err != nil {
+		return nil
+	}
+	return bs
 }
+
+
+// func NewResource(b []byte) (Resource, error) {
+// 	r := Resource{}
+// 	err := json.Unmarshal(b, &r)
+// 	return r, err
+// }
